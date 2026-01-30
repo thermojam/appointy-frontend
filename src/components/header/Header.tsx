4aa9098822a76fa504b5d8bec7c35bfe9f947507
thermojam <nmensky@gmail.com>
2026-01-30 19:12:16 +0300

    hiding the header on routes with the master panel

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { ThemeToggle } from "./ThemeToggle"
 import { Logo } from "./Logo"
 
 export function Header() {
+    const pathname = usePathname();
+
+    if (pathname.startsWith('/dashboard')) {
+        return null;
+    }
     return (
         <header className="max-w-[1400px] my-[20px]  mx-auto px-4 flex items-center justify-between">
             <Logo />
